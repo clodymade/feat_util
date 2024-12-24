e34541b0f61705144c14d9dc8b941c91c37b6647
netcanis <netcanis@gmail.com>
2024-12-24 17:46:21 +0900

    update jvm version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,12 +32,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17//21
-        targetCompatibility = JavaVersion.VERSION_17//21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "17"//"21"
+        jvmTarget = "17"
     }
 
     buildFeatures {
